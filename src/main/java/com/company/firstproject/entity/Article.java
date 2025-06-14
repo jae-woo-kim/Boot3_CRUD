994b441989e2b7383ed708bd2c35dfed36061b3f
lmcomputer <lmcomputer@naver.com>
2025-06-14 22:12:43 +0900

    Check File

---
+++ src/main/java/com/company/firstproject/entity/Article.java
@@ -0,0 +1,30 @@
+package com.company.firstproject.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+@Entity
+public class Article {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)  // DB가 id 자동 생성
+    private Long id;
+
+    private String title;
+
+    private String content;
+
+    public void patch(Article article) {
+        if(article.title != null)
+            this.title = article.title;
+        if(article.content != null)
+            this.content = article.content;
+    }
+}
---
+++ src/test/java/com/company/firstproject/service/ArticleServiceTest.java
@@ -0,0 +1,75 @@
+package com.company.firstproject.service;
+
+import com.company.firstproject.dto.ArticleForm;
+import com.company.firstproject.entity.Article;
+import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+class ArticleServiceTest {
+
+    @Autowired
+    ArticleService articleService;
+
+    @Test
+    @Transactional
+    void index() {
+        // 1. 예상 데이터
+        Article a = new Article(1L, "가가가가", "1111");
+        Article b = new Article(2L, "나나나나", "2222");
+        Article c = new Article(3L, "다다다다", "3333");
+        List<Article> expected = new ArrayList<>(Arrays.asList(a, b, c));
+        // 2. 실제 데이터
+        List<Article> articles = articleService.index();
+        // 3. 비교 및 검증
+        assertEquals(expected.toString(), articles.toString());
+    }
+
+
+    @Test
+    void show_성공_존재하는_id_입력() {
+        // 1. 예상 데이터
+        Long id = 1L;
+        Article expected = new Article(id, "가가가가", "1111");
+        // 2. 실제 데이터
+        Article article = articleService.show(id);
+        // 3. 비교 및 검증
+        //assertEquals(expected.toString(), article.toString());
+        assertThat(expected.toString()).isEqualTo(article.toString());
+    }
+
+    @Test
+    void show_실패_존재하는_id_입력() {
+        // 1. 예상 데이터
+        Long id = -1L;
+        Article expected = null;
+        // 2. 실제 데이터
+        Article article = articleService.show(id);
+        // 3. 비교 및 검증
+        assertEquals(expected, article);
+    }
+
+    @Test
+    @Transactional
+    void create_성공_title과_content만_있는_dto_입력() {
+        // 1. 예상 데이터
+        String title = "라라라라";
+        String content = "4444";
+        ArticleForm dto = new ArticleForm(null, title, content);
+        Article expected = new Article(4L, title, content);
+        // 2. 실제 데이터
+        Article article = articleService.create(dto);
+        // 3. 비교 및 검증
+        assertEquals(expected.toString(), article.toString());
+    }
+
+}
